b929f88636ae2bc0df6d17997f90c7a4f728cab0
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2019-03-21 08:24:27 +0100

    Add the profile model

--- counter/models.py
+++ counter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total = models.FloatField(null=True, blank=True, default=0.0)
+    key = models.CharField(max_length=10, unique=True, default="key")
+
+    def __str__(self):
+        return self.user.username
